7323b32a5850012ecdc17ee4493f5756a7593e91
RyokuItaro <39265720+RyokuItaro@users.noreply.github.com>
2022-04-09 12:30:59 +0200

    templatka z zajec do demonow

--- main.cpp
+++ main.cpp
@@ -1,8 +1,47 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+int main(void) {
+        
+        pid_t pid, sid;
+        
+        pid = fork();
+        if (pid < 0) {
+                exit(EXIT_FAILURE);
+        }
+        
+        if (pid > 0) {
+                exit(EXIT_SUCCESS);
+        }
+        umask(0);
+                
+        sid = setsid();
+        if (sid < 0) {
+                /* Log the failure */
+                exit(EXIT_FAILURE);
+        }
+        
+        
+        /* Change the current working directory */
+        if ((chdir("/")) < 0) {
+                /* Log the failure */
+                exit(EXIT_FAILURE);
+        }
+        
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+        
+        while (1) {
 
-using namespace std;
-
-int main(){
-    printf("helloworld");
-    return 0;
+           
+           sleep(15);
+        }
+   exit(EXIT_SUCCESS);
 }
